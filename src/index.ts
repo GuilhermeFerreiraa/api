1684fb7b872c771ecbbd3f11d3178e9d4243d03c
guilhermeferreiraa <gui.2001@hotmail.com>
2024-04-04 22:24:21 -0300

    feat: only run server on db up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-app.listen(3001, () => {
-  console.log('🚀 server is running');
-});
+    app.listen(port, () => {
+      console.log(`🚀 server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => console.error(err));
