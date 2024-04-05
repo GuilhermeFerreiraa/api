6ea4845ea34dfe2297c9959b63d830408f4443f9
guilhermeferreiraa <gui.2001@hotmail.com>
2024-04-04 22:25:49 -0300

    fix: remove index from src

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var app = (0, express_1.default)();
-app.listen(3001, function () {
-    console.log('🚀 server is running on http://localhost:3001');
-});
